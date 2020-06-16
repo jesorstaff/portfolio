e29127705c3efec8674d39787ac2cac50d98b96d
jesortstaff <jesorstaff@gmail.com>
2020-06-16 15:57:29 +0500

    изменения 2020

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import { AwesomeButtonProgress } from 'react-awesome-button';
 import 'react-awesome-button/dist/styles.css';
@@ -7,39 +7,37 @@ import './App.css';
 import PopupComponent from './components/Menubar/PopupComponent';
 import Follow from './components/Follow';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <PopupComponent />
-        <Follow />
-        <div className="my-about-text">
-            <h1>Привет, меня зовут Тимур Байрамгулов.</h1>
-            <h2>Я являюсь <b>Junior JavaScript Web Developer.</b> Посмотрите мои работы на React JS <Link to="/apps"><b>здесь</b></Link></h2>
-            <h3>Так же вы можете посмотреть последние мои проекты на странице <Link to="/portfolio"><b>портфолио.</b></Link>
-            </h3>
-            <h3>Связь со мной через <a href="tg://resolve?domain=<@Timagood>">Telegram</a></h3>
-        </div>
+function App() {
+  return (
+    <div className="App">
+      <PopupComponent />
+      <Follow />
+      <div className="my-about-text">
+        <h1>Привет, меня зовут Тимур Байрамгулов.</h1>
+        <h2>Я являюсь <b>Junior JavaScript Web Developer.</b> Посмотрите мои работы на React JS <Link to="/apps"><b>здесь</b></Link></h2>
+        <h3>Так же вы можете посмотреть последние мои проекты на странице <Link to="/portfolio"><b>портфолио.</b></Link>
+        </h3>
+        <h3>Связь со мной через <a href="tg://resolve?domain=<@Timagood>">Telegram</a></h3>
         <div className="app-btn">
-            <Link to="/portfolio">
-                <AwesomeButtonProgress
-                    size="200px"
-                    type="primary"
-                    loadingLabel="Загрузка.."
-                    resultLabel="Готово !"
-                    action={(element, next) => {
-                        setTimeout(() => {
-                            next();
-                        }, 1500);
-                    }}
-                >
-                    Перейти к портфолио
-                </AwesomeButtonProgress>
-            </Link>
+          <Link to="/portfolio">
+            <AwesomeButtonProgress
+              size="200px"
+              type="primary"
+              loadingLabel="Загрузка.."
+              resultLabel="Готово !"
+              action={(element, next) => {
+                setTimeout(() => {
+                  next();
+                }, 1500);
+              }}
+            >Перейти к портфолио</AwesomeButtonProgress>
+          </Link>
         </div>
       </div>
-    );
-  }
+    </div>
+  )
 }
+
+    
 
 export default App;
--- src/components/Follow.js
+++ src/components/Follow.js
@@ -1,33 +1,38 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-
-class Follow extends Component {
-    render() {
-        return (
-            <div className="follow-me">
-                <ul>
-                    <li>
-                        <small>Подписаться</small>
-                    </li>
-                    <li>
-                        <a href="https://www.facebook.com/timur.bairamgulov" className="vk" title="Facebook" target="_blank" rel="noopener noreferrer">
-                          <span>Follow to facebook</span>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="https://github.com/jesorstaff" className="github" title="Гитхаб" target="_blank" rel="noopener noreferrer">
-                            <span>Follow to github</span>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="https://www.instagram.com/jesorstaff/" className="instagram" title="Инстаграм" target="_blank" rel="noopener noreferrer">
-                            <span>Follow to Instagram</span>
-                        </a>
-                    </li>
-                </ul>
-            </div>
-        )
-    }
+const Follow = () => {
+    return (
+        <div className="follow-me">
+            <small>Подписаться</small>
+            <SocialLink 
+                href="https://www.facebook.com/timur.bairamgulov"
+                className="vk"
+                title="Facebook"
+            />
+            <SocialLink 
+                href="https://github.com/jesorstaff"
+                className="github"
+                title="GitHub"
+            />
+            <SocialLink 
+                href="https://www.instagram.com/jesorstaff/"
+                className="instagram"
+                title="Instagram"
+            />
+        </div>
+    )
 }
 
-export default Follow
+const SocialLink = (props) => {
+    return (
+        <a 
+            href={props.href} 
+            className={props.className} title={props.title} 
+            target="_blank" 
+            rel="noopener noreferrer">
+            <span>{`Follow to ${props.title}`}</span>
+        </a>
+    )
+}
+
+export default Follow;
